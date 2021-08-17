d4f00e23c5962397473c59739d0212080f174b75
iswahyudi <60358147+yudipd@users.noreply.github.com>
2021-08-18 00:07:11 +0700

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/infor2018/biodatadirisqlite/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.infor2018.biodatadirisqlite;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.infor2018.biodatadirisqlite";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
